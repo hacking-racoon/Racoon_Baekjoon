f81ef9e62724f71af081f87cb0693cf7d06dbb8a
Hyungjin Cha <55372297+hacking-racoon@users.noreply.github.com>
2023-02-17 20:17:16 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <deque>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')"  << endl;
+	cout << "(  /  )"   << endl;
+	cout << " \\(__)|" << endl;
+}
