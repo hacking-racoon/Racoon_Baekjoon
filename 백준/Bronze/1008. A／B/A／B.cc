295354bac14ebe7fc0e0a38df451e35493cb73e6
Hyungjin Cha <55372297+hacking-racoon@users.noreply.github.com>
2023-02-17 20:07:40 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <deque>
+using namespace std;
+
+int main()
+{
+	long double A, B;
+	cin >> A >> B;
+
+	cout.precision(15);
+	cout << A / B << endl;
+}
