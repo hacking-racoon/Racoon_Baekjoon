e36d3eb008a3cacb014c90a4b57e4960891eae5d
Hyungjin Cha <55372297+hacking-racoon@users.noreply.github.com>
2023-03-02 20:33:42 +0900

    [Silver I] Title: 캡틴 이다솜, Time: 56 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Silver/1660. 캡틴 이다솜/캡틴 이다솜.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+using namespace std;
+
+#define m1in(a, b) ((a) > (b) ? (b) : (a))
+typedef unsigned long long ull;
+ull cntX[130];
+
+ull calcCntX(ull len)
+{
+	return ((len * len) + len) / 2;
+}
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	for (int i = 1; i < 130; ++i)
+	{
+		cntX[i] = cntX[i - 1] + calcCntX(i);
+	}
+
+	vector<ull> DP(N + 1, 987654321);
+	DP[0] = 0;
+
+	ull currCntX;
+	for (int i = 1; i <= N; ++i)
+	{
+		int len = 1;
+		while (true) {
+			currCntX = cntX[len];
+			if (currCntX > i) break;
+
+			DP[i] = m1in(DP[i], DP[i - currCntX] + 1);
+			len++;
+		}
+	}
+
+	cout << DP[N] << endl;
+}
