fd4f9bd7717f82bca5d06db18947f0e567832c47
Hyungjin Cha <55372297+hacking-racoon@users.noreply.github.com>
2023-03-16 23:12:45 +0900

    [Silver I] Title: Subway, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/21959. Subway/README.md
@@ -0,0 +1,28 @@
+# [Silver I] Subway - 21959 
+
+[문제 링크](https://www.acmicpc.net/problem/21959) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 4 ms
+
+### 분류
+
+수학, 트리, 구성적, 조합론
+
+### 문제 설명
+
+<p>Given an integer number K, generate a tree with minimum number of nodes such that there are exactly K pairs of nodes (X, Y), where X is an ancestor of Y.</p>
+
+### 입력 
+
+ <p>The input (from the console) will contain a single integer number, K – the number of pairs with the specified property.</p>
+
+### 출력 
+
+ <p>The output (to the console) will contain N+1 lines, representing the generated tree, the nodes being indexed from 0.</p>
+
+<p>The first line will contain the number N – the number of nodes in the tree.</p>
+
+<p>The following N lines will contain each 2 numbers X and T, separated by a space, with the following meaning: node T is the direct ancestor of node X. If node X doesn’t have a direct ancestor, T will have value -1.</p>
+
---
+++ 백준/Silver/21959. Subway/Subway.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <queue>
+#include <functional>
+#include <queue>
+#include <math.h>
+#include <queue>
+using namespace std;
+#define m1in(a,b) ((b) > (a) ? (a) : (b))
+#define m1ax(a,b) ((b) > (a) ? (b) : (a))
+
+typedef unsigned long long ull;
+
+ull inline nC2(int n)
+{
+	return n * (n - 1) / 2;
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int K;
+	cin >> K;
+
+	int currIdx = 1;
+	int currNode;
+	int ret = 0;
+	ull currComb;
+	vector<int> combs;
+	while (K != 0)
+	{
+		currComb = 1;
+		currNode = 2;
+
+		while ((currComb = nC2(currNode) <= K))
+		{
+			currNode++;
+		}
+		combs.push_back(currNode-2);
+		ret += currNode - 2;
+		K -= nC2(currNode-1);
+	}
+
+	cout << ret + 1 << endl;
+	cout << "0 -1" << endl;
+
+	for (const auto& elem : combs)
+	{
+		for (int i = 0; i < elem; ++i)
+		{
+			if (i == 0)
+			{
+				cout << currIdx++ << " 0" << endl;
+			}
+			else
+			{
+				cout << currIdx << " " << currIdx - 1 << endl;
+				currIdx++;
+			}
+		}
+	}
+}
